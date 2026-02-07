899780d30e331eb2c9710ac5287c28ede459a522
syncrexwen <syncrexwen@gmail.com>
2026-02-07 17:00:04 +0800

    Update index.astro content

--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,9 @@
+// Global constants for the site
+
+// This is the title rendered in the header and used as the default SEO title
 export const SITE_TITLE = 'Syncrex Wen';
+// This is the description meta tag content and used as the default SEO description
 export const SITE_DESCRIPTION =
     'This is my personal blog mainly about healthcare, artificial intelligence.';
+// This is the name displayed in the footer copyright information
 export const AUTHOR = 'Kangcheng Wen';
